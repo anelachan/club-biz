e5e3ee2cf7319b395cae33a73de77787899e82a8
anela <anelachan@gmail.com>
2014-05-17 10:35:12 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClubBiz::Application.config.secret_key_base = 'cfe333bcb82d08d1d04aa5a8b01cc3f3de4afebe9095a9c69180cf1b560668deb7b38120bd5eee8dad32d643360f559873cbbb3a4b71756b0cc719e3d548781a'
